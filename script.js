b0fd273fe776dc393d5badb5a6422d9f2b206646
Fourllan <krab336@mail.ru>
2022-10-24 01:22:05 +0300

    secondDay

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-window.addEventListener("DOMContentLoaded", ()=>{
+window.addEventListener('load', () => {
 
-  const slides = document.querySelectorAll('.slide');
+  const item = document.querySelector('.item');
+  const placeholders = document.querySelectorAll('.placeholder');
 
-  for(const slide of slides) {
-    slide.addEventListener('click', (e)=>{
-      clearActiveCLasses();
-      slide.classList.add('active');
-      
-    });
+  item.addEventListener('dragstart', dragstart);
+  item.addEventListener('dragend', dragend);
+  
+  for (let placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover);
+    placeholder.addEventListener('dragenter', dragenter);
+    placeholder.addEventListener('dragleave', dragleave);
+    placeholder.addEventListener('drop', drop);
   }
 
-  function clearActiveCLasses() {
-    slides.forEach(element => {
-      element.classList.remove('active');
-    });
+  function dragstart(e) {
+    e.target.classList.add('hold');
+    setTimeout(() => {
+      e.target.classList.add('hide');
+    }, 0);
 
+  }
+
+  function dragend(e) {
+    e.target.className = "item";
+  }
+
+  function dragover(e) {
+    e.preventDefault();
+  }
+
+  function dragenter(e) {
+    e.target.classList.add('hovered');
+  }
+
+  function dragleave(e) {
+    e.target.classList.remove('hovered');
+  }
+
+  function drop(e) {
+    e.target.classList.remove('hovered');
+    e.target.append(item);
   }
 
 });
